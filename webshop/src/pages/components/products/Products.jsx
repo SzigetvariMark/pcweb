f685e37fea4cd2f880f898cb70e1333be6b85985
Mate Brosztl <brosztl.mate@gmail.com>
2023-12-17 16:32:13 +0100

    add second row products to home page

--- webshop/src/pages/components/products/Products.jsx
+++ webshop/src/pages/components/products/Products.jsx
@@ -3,6 +3,8 @@ import './Products.css'
 
 function Products() {
   return (
+
+    <>
     <section className='section-products'>
         <div className="product">
             <button className="product-gpu">
@@ -23,6 +25,30 @@ function Products() {
         </div>
 
     </section>
+
+    <section className='section-products-second'>
+        <div className="product">
+            <button className="product-gpu">
+                <label htmlFor="text">GPU</label>
+            </button>
+        </div>
+
+        <div className="product">
+            <button className="product-cpu">
+                <label htmlFor="text">CPU</label>
+            </button>
+        </div>
+
+        <div className="product">
+            <button className="product-ram">
+                <label htmlFor="text">Ram</label>
+            </button>
+        </div>
+
+    </section>
+    </>
+    
+    
   )
 }
 
