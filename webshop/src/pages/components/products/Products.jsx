dc8a8773d76f4a06dcd5a018a2e58a91b464dca3
Mate Brosztl <brosztl.mate@gmail.com>
2023-12-17 16:07:08 +0100

    Home page cpu products and cpu picture finished

--- webshop/src/pages/components/products/Products.jsx
+++ webshop/src/pages/components/products/Products.jsx
@@ -1,33 +1,21 @@
 import React from 'react'
 import './Products.css'
-import gpu from "/img/gpu.avif";
 
 function Products() {
   return (
     <section className='section-products'>
         <div className="product">
-            <button className="product-info">
+            <button className="product-gpu">
                 <label htmlFor="text">GPU</label>
             </button>
         </div>
 
         <div className="product">
-            <div className="">
-                <h2>Desktop PC 2</h2>
-                <p>High-performance desktop computer for gaming and work.</p>
-                <p>Price: $1299</p>
-                <input type="submit" value="Add to cart" />
-            </div>
+            <button className="product-cpu">
+                <label htmlFor="text">CPU</label>
+            </button>
         </div>
 
-        <div className="product">
-            <div className="">
-                <h2>Desktop PC 1</h2>
-                <p>Powerful desktop computer with the latest hardware.</p>
-                <p>Price: $999</p>
-                <input type="submit" value="Add to cart" />
-            </div>
-        </div>
     </section>
   )
 }
