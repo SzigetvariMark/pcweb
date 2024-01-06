a0b30684b6ace173d12ab08e7bc2761388e7c62a
Mate Brosztl <brosztl.mate@gmail.com>
2024-01-06 17:42:20 +0100

    töltsed le 1 sort változtattam

--- SQLserver/pcwebshop.sql
+++ SQLserver/pcwebshop.sql
@@ -17,7 +17,7 @@ CREATE TABLE `orders` (
     `TotalCost` varchar(250) NOT NULL,
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
-CREATE TABLE `payments` (
+CREATE TABLE `pcwebshop`.`payments` (
   `PaymentID` int(11) NOT NULL,
   `OrderIDFk` int(20) DEFAULT NULL,
   `PaymentMethod` varchar(50) NOT NULL
