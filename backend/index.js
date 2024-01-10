cda508ed22f73495b8b691c22ced3c080bf79981
Mate Brosztl <brosztl.mate@gmail.com>
2024-01-10 19:38:57 +0100

    add get products/error code

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,29 @@
-import express from "express";
+import express from "express"
 import mysql from "mysql"
 
 const app = express()
 
-const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"webshop"
+
+const db = mysql.createConnection ({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "pcwebshop"
 })
 
-app.listen(8800, () =>{
+app.get("/", (req, res) => {
+    res.json("hello this is the backend")
+})
+
+app.get("/products", (req, res) => {
+    const q = "SELECT * FROM products"
+    db.query(q, (err,data)=> {
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+
+app.listen(8800, () => {
     console.log("Connected to backend!")
 })
