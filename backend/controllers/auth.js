1d3a0b542b6f8197875837a6449f929bccc9a5bf
Mate Brosztl <brosztl.mate@gmail.com>
2024-01-13 23:34:06 +0100

    add login to the backend

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -23,9 +23,24 @@ export const register = (req, res) => {
     db.query(q, [values], (err, data) => {
       if (err) return res.json(err);
 
-      return res.status(200).jsnon("User has been created.");
+      return res.status(200).json("User has been created.");
     });
   });
 };
-export const login = (req, res) => {};
+export const login = (req, res) => {
+  const q = "SELECT * FROM customers WHERE Email = ?";
+
+  db.query(q, [req.body.Email], (err, data) => {
+    if (err) return res.json(err);
+    if (data.length === 0) return res.status(404).json("User not found!");
+
+    const isPasswordCorrect = bcrypt.compareSync(
+      req.body.PasswordHash,
+      data[0].PasswordHash
+    );
+
+    if (!isPasswordCorrect)
+      return res.status(400).json("Wrong email or password!");
+  });
+};
 export const logout = (req, res) => {};
