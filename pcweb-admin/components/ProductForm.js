7dec939a9c447ddd0e06c0d2c96f9dd403dbe469
Brösztl Máté 826 <bromat826@hengersor.hu>
2024-01-25 12:46:01 +0100

    add photo upload to admin

--- pcweb-admin/components/ProductForm.js
+++ pcweb-admin/components/ProductForm.js
@@ -7,6 +7,7 @@ export default function ProductForm({
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
+  images,
 }) {
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
@@ -36,6 +37,28 @@ export default function ProductForm({
         value={title}
         onChange={(ev) => setTitle(ev.target.value)}
       />
+      <label>Photos</label>
+      <div className="mb-2">
+        <label className="w-24 h-24 text-center flex items-center justify-center rounded-xl text-gray-500 bg-gray-200 cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15"
+            />
+          </svg>
+          <div>Upload</div>
+          <input type="file" className="hidden" />
+        </label>
+        {!images?.lenght && <div>No photo in this product</div>}
+      </div>
       <label>Product description</label>
       <textarea
         placeholder="description"
