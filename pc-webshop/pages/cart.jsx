0412d02aa549d91c80a386f3f42c045a1d797868
Brösztl Máté 826 <bromat826@hengersor.hu>
2024-03-06 11:49:05 +0100

    cart

--- pc-webshop/pages/cart.jsx
+++ pc-webshop/pages/cart.jsx
@@ -13,11 +13,13 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { useSession } from "next-auth/react";
 
 const cart = () => {
   const router = useRouter();
   const { cartProducts } = useContext(CartContext);
   const [products, setProducts] = useState([]);
+  const { data: session } = useSession();
 
   useEffect(() => {
     if (cartProducts.length > 0) {
@@ -33,7 +35,7 @@ const cart = () => {
 
   return (
     <div className="gap-12 grid grid-cols-3 w-full mt-10">
-      <div className="col-span-2 bg-blue-500 rounded-lg p-8">
+      <div className="col-span-2 bg-white shadow-xl rounded-lg p-8">
         {!cartProducts?.length && (
           <div>
             <p className="title">Üres a kosarad</p>
@@ -42,19 +44,19 @@ const cart = () => {
         {products?.length > 0 && (
           <>
             <h2 className="title">Kosár</h2>
-            {products.map((items, index) => (
-              <div key={index}>
-                <Table>
-                  <TableCaption>A legutóbbi hozzáadott termékek</TableCaption>
-                  <TableHeader>
-                    <TableRow>
-                      <TableHead className="w-[100px]">Termék képe</TableHead>
-                      <TableHead>Termék neve</TableHead>
-                      <TableHead>Ár</TableHead>
-                      <TableHead className="text-right">Összeg</TableHead>
-                    </TableRow>
-                  </TableHeader>
-                  <TableBody>
+            <div>
+              <Table>
+                <TableCaption>A legutóbbi hozzáadott termékek</TableCaption>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="w-[100px]">Termék képe</TableHead>
+                    <TableHead>Termék neve</TableHead>
+                    <TableHead>Ár</TableHead>
+                    <TableHead className="text-right">Összeg</TableHead>
+                  </TableRow>
+                </TableHeader>
+                {products.map((items, index) => (
+                  <TableBody key={index}>
                     <TableRow>
                       <TableCell className="font-medium">
                         <img src={items.images} alt="kep" />
@@ -66,9 +68,9 @@ const cart = () => {
                       </TableCell>
                     </TableRow>
                   </TableBody>
-                </Table>
-              </div>
-            ))}
+                ))}
+              </Table>
+            </div>
             <Button variant="outline" className="float-end" onClick={goBack}>
               Vásárlás folytatása
             </Button>
@@ -76,13 +78,42 @@ const cart = () => {
         )}
       </div>
       {!!cartProducts?.length && (
-        <div className="bg-blue-400 rounded-lg p-8">
-          <h2 className="title">Szállitási információk</h2>
-          <Input className="mt-4" />
-          <Input className="mt-4" />
+        <div className="bg-white shadow-xl rounded-lg p-8">
+          <h2 className="title text-center">Szállitási információk</h2>
+          <Input
+            type="text"
+            className="mt-4 hover:border-orange-700"
+            placeholder={session?.user.name}
+            name={session?.user.name}
+            readOnly={"readonly"}
+          />
+          <Input
+            type="email"
+            className="mt-4 hover:border-orange-700"
+            placeholder={session?.user.email}
+            name={session?.user.email}
+            readOnly={"readonly"}
+          />
+          <Input
+            type="number"
+            className="mt-4 hover:border-orange-700"
+            placeholder="Telefonszám"
+          />
+          <Input
+            type="text"
+            className="mt-4 hover:border-orange-700"
+            placeholder="Cím"
+          />
+          <div className="flex gap-1 mt-4">
+            <Input type="text" placeholder="Emelet" />
+            <Input type="text" className="" placeholder="Ajtó" />
+          </div>
           <Button variant="ghost" className="w-full border text-lg mt-5">
             Tovább a fizetéshez
           </Button>
+          <p className="float-right text-xs font-light italic mt-9 text-orange-700">
+            *A sárga mezőket kötelező kitölteni!
+          </p>
         </div>
       )}
     </div>
