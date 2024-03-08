7e89c63660de31ae5e2748351a68ed306ef26204
Nagy Bence 358 <nagben358@hengersor.hu>
2024-03-08 11:16:10 +0100

    Auction data from database

--- pc-webshop/components/AuctionsFeatured.jsx
+++ pc-webshop/components/AuctionsFeatured.jsx
@@ -1,47 +1,41 @@
+import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
+import axios from "axios";
 
 const AuctionsFeatured = () => {
+  const [auctionData, setAuctionData] = useState([]);
+  useEffect(() => {
+    axios.get("/api/auction").then((respone) => {
+      try {
+        setAuctionData(respone.data);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  }, []);
   return (
     <>
       <div className="flex gap-64 p-10 items-center rounded-lg mt-10 ">
-        <div className="">
-          <h2 className="title">Asus madafaka</h2>
-          <div className="flex gap-4">
-            <p>5000FT</p>
-            <p>4 licit</p>
-            <p>3óra 12perc</p>
-          </div>
-          <Button className="mt-2">Licitálok</Button>
-        </div>
-        <div>
-          <button>
-            <img
-              src="img/IMG_1487-removebg-preview.png"
-              alt="kep"
-              className="w-80"
-            />
-          </button>
-        </div>
-      </div>
-      <div className="flex gap-64 p-10 items-center rounded-lg mt-10 ">
-        <div className="">
-          <h2 className="title">Asus madafaka</h2>
-          <div className="flex gap-4">
-            <p>5000FT</p>
-            <p>4 licit</p>
-            <p>3óra 12perc</p>
-          </div>
-          <Button className="mt-2">Licitálok</Button>
-        </div>
-        <div>
-          <button>
-            <img
-              src="img/IMG_1487-removebg-preview.png"
-              alt="kep"
-              className="w-80"
-            />
-          </button>
-        </div>
+        {auctionData.map((items) => (
+          <>
+            <div className="">
+              <div>
+                <h2 className="title">{items.title}</h2>
+                <div className="flex gap-4">
+                  <p>{items.price}Ft</p>
+                  <p>4 licit</p>
+                  <p>3óra 12perc</p>
+                </div>
+                <Button className="mt-2">Licitálok</Button>
+              </div>
+            </div>
+            <div>
+              <button>
+                <img src={items.images} alt="kep" className="w-60" />
+              </button>
+            </div>
+          </>
+        ))}
       </div>
     </>
   );
