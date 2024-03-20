dd56f3fc3dbc53c85f51df7c871d621893d0f505
unknown <szigetvarimarkjanos@gmail.com>
2024-03-20 11:23:33 +0100

    page update

--- pc-webshop/components/SinglePage.jsx
+++ pc-webshop/components/SinglePage.jsx
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "./ui/button";
 
 const SinglePage = ({ product }) => {
+  const [allImg, setAllImg] = useState(product.images);
+
   return (
-    <div className="mt-6 flex">
-      <div>
-        <img src={product.images[0]} alt="kep" className="border h-72" />
+    <>
+      <div className="mt-6 flex justify-around w-full">
+        <div className="shadow-xl p-10 rounded-xl">
+          <img src={product.images[0]} alt="kep" className="h-60" />
+          <button className="flex gap-10 mt-6">
+            {allImg.map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt="kep"
+                className="h-20 hover:border-2 transition-transform border-gray-500 p-2 rounded-sm"
+              />
+            ))}
+          </button>
+        </div>
+        <div className="shadow-xl p-10 rounded-xl flex flex-col justify-between">
+          <div>
+            <h1 className="title">{product.title}</h1>
+            <p className="text-2xl font-bold text-red-500">{product.price}Ft</p>
+            <p>Raktáron: 3db</p>
+          </div>
+          <div>
+            <Button variant="default" className="w-full text-lg">
+              Kosárba
+            </Button>
+          </div>
+        </div>
       </div>
-      <div>
-        <h1>Neve: {product.title}</h1>
-        <p>Ára: {product.price}</p>
+      <div className="mt-6 shadow-xl p-10">
+        <h2 className="title">Leírás:</h2>
+        <p className="mt-2">Leírás: {product.description}</p>
       </div>
-    </div>
+    </>
   );
 };
 
