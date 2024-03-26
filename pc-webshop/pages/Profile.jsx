10aa33f203e8a5f75ae7724d565830b0b33638fe
Mate Brosztl <brosztl.mate@gmail.com>
2024-03-26 20:00:36 +0100

    finished profile resp

--- pc-webshop/pages/Profile.jsx
+++ pc-webshop/pages/Profile.jsx
@@ -49,7 +49,7 @@ const Profile = () => {
 
   return (
     <div className="flex-col">
-      <div className="flex gap-32 p-6 mt-6 shadow-xl border rounded-lg">
+      <div className="flex gap-32 p-6 mt-6 shadow-xl border rounded-lg max-lg:flex-col max-lg:gap-5 max-lg:items-center">
         <div>
           <h3 className=" bg-white/20 border-l-2 border-blue-400 rounded-md p-2 mb-4 font-semibold text-2xl">
             Alapvető információ
@@ -74,7 +74,7 @@ const Profile = () => {
             <FaPenAlt fontSize={16} className="cursor-pointer" />
           </div>
         </div>
-        <div className="flex flex-col justify-right items-center mr-20">
+        <div className="flex flex-col justify-right items-center mr-20 max-lg:flex-row max-lg:gap-2 max-lg:text-lg max-lg:font-semibold">
           <img
             src={session?.user.image}
             alt="profile picture"
@@ -90,33 +90,33 @@ const Profile = () => {
           <h3 className=" bg-white/20 border-l-2 border-blue-400 rounded-md p-2 mb-4 font-semibold text-2xl">
             Rendelési Információk
           </h3>
-          <div className="max-xl:flex max-xl:gap-4 max-xl:justify-center max-xl:items-center">
+          <div className="max-lg:flex max-lg:gap-8 max-lg:justify-center max-lg:items-center max-sm:flex-col">
             {deliveryInformation.map((items, index) => (
               <div
                 key={index}
-                className="flex justify-between mb-2 items-center max-xl:flex-col max-xl:gap-2 max-xl:items-start "
+                className="flex justify-between mb-2 items-center max-lg:flex-col max-lg:gap-2 max-lg:items-start "
               >
-                <div className="max-xl:flex max-xl:items-center">
+                <div className="profileOrderList">
                   <p className="Title2">Város:</p>
                   <label className="font-light">{items.city}</label>
                 </div>
-                <div className="max-xl:flex max-xl:items-center">
+                <div className="profileOrderList">
                   <p className="Title2">Cim:</p>
                   <label className="font-light">{items.address}</label>
                 </div>
-                <div className="max-xl:flex max-xl:items-center">
+                <div className="profileOrderList">
                   <p className="Title2">Irányitószám:</p>
                   <label className="font-light">{items.postal}</label>
                 </div>
-                <div className="max-xl:flex max-xl:items-center">
+                <div className="profileOrderList">
                   <p className="Title2">Emelet:</p>
                   <label className="font-light">{items.floor}</label>
                 </div>
-                <div className="max-xl:flex max-xl:items-center">
+                <div className="profileOrderList">
                   <p className="Title2">Ajtó:</p>
                   <label className="font-light">{items.door}</label>
                 </div>
-                <div className="max-xl:flex-col max-xl:items-center">
+                <div className="profileOrderList">
                   <p className="Title2">Amiket rendelt:</p>
                 </div>
                 <Button
